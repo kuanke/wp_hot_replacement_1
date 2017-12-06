037a6dfcf5aab58e5a4a91fa14202f54cfb8a1e6
zhenglonglong <792065@nd.com>
2017-12-06 22:17:36 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/' //以确保文件资源能够在 http://localhost:<端口号> 下正确访问
+        // 声明资源（js、css、图片等）的引用路径
+        // webpack打包时会把html页面上的相对路径根据publicPath解析成绝对路径
+        // eg：当publicPath为'https://jd.com/'时，如果有html或者css含有一张图片相对路径为'./img/test.jpg',打包之后html（或css）中图片的路径就会变成'https://jd.com/img/test.jpg'
+      publicPath: '/'
     }
   };
